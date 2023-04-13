8e840b38f0e29d4f99429e4aaf88716167c495c1
Jordan Hancock <jhancock@chicagobooth.edu>
2023-04-13 15:28:13 +0000

    Subtraction completed

--- app/controllers/math_controller.rb
+++ app/controllers/math_controller.rb
@@ -9,4 +9,17 @@ class MathController < ApplicationController
   def add_form
     render({:template => "math_templates/addition_form.html.erb"})
   end
+
+  def subtract_results
+    @first = params.fetch("first_num").to_f
+    @second = params.fetch("second_num").to_f
+
+    @result = @second - @first
+
+    render({ :template => "math_templates/subtraction_results.html.erb"})
+  end
+
+  def subtract_form
+    render({ :template => "math_templates/subtraction_form.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  #Addition
   get("/wizard_add", { :controller => "math", :action => "add_results"})
-
   get("/add", { :controller => "math", :action => "add_form"})
+
+  #Subtraction
+  get("/wizard_subtract", { :controller => "math", :action => "subtract_results"})
+  get("/subtract", { :controller => "math", :action => "subtract_form"})
+
+
 end
